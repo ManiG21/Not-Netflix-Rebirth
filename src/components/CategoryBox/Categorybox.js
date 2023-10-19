1cf4fc2c849cc84c825ac0c502412f680250a39e
ManiG21 <manigrant023@gmail.com>
2023-10-19 00:32:03 -0400

    clean

--- src/components/CategoryBox/Categorybox.js
+++ src/components/CategoryBox/Categorybox.js
@@ -5,7 +5,6 @@ import { useEffect, useState } from 'react';
 import { getDataApi } from '../../network-requests';
 
 
-
 export default function Categorybox({genres}) {
     let { id } = useParams();
     const [moviesList, setMoviesList] = useState();
@@ -20,7 +19,6 @@ export default function Categorybox({genres}) {
     return  <div className="categorybox">
       <header>{genres?.find(genre => genre.id == id)?.name}</header>
       <MovieList moviesList= {moviesList}/>
-
 
 
     </div>
